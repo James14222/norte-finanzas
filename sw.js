b35db746cb3254c669d7d8862cfeb9382b67e80d
James14222 <157189360+James14222@users.noreply.github.com>
2026-09-18 10:44:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Estrategia: Cache-first para el app shell, network-first para Firebase/APIs.
  */
 
-const CACHE_NAME = 'norte-v129';
+const CACHE_NAME = 'norte-v131';
 const BASE = '';
 
 // Recursos del app shell que se cachean en la instalación
